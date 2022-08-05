d78a840495ff0286707f274b558b31fbdcfc5677
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2022-08-05 23:19:01 +0200

    initial commit

---
+++ playwright.config.ts
@@ -0,0 +1,42 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+require('dotenv').config();
+
+const config: PlaywrightTestConfig = {
+  testDir: './e2e',
+  timeout: 30 * 1000, // maximum time one test can run for
+  expect: { // 
+    timeout: 5000
+  },
+  fullyParallel: true,
+  reporter: 'html',
+  use: {
+    baseURL: process.env.BASE_URL,
+    headless: false,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
+  ],
+};
+
+export default config;
